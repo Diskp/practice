5a7a92f3f93c24788222f04f8391d3b1177860d1
hide <981392570@qq.com>
2022-08-25 22:19:32 +0800

    TEST

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
-	testRemoveDuplicates()
+	//testRemoveDuplicates()
+	var a int16 = 4
+	var b int32 = 8
+	var c int64
+	c = int64(a) + int64(b)
+	fmt.Println(c)
 }
